aeb4c776ab33d143e51945e5e49a2a75ceb72f73
Thomas Waldorf <zeeroh71@gmail.com>
2021-07-17 07:41:49 -0400

    Added Firebase config info to process.env

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,12 +4,12 @@ import "firebase/auth";
 import 'firebase/storage';
 
 const config = {
-  apiKey: "AIzaSyALt3V0U_9A6DZBmducVWvAchE3XUG6qdY",
-  authDomain: "vuefs-cust.firebaseapp.com",
-  databaseURL: "https://vuefs-cust.firebaseio.com",
-  projectId: "vuefs-cust",
-  storageBucket: "vuefs-cust.appspot.com",
-  messagingSenderId: "464183442563",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
